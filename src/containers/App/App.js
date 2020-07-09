bab1cf5708f1d8c5f5179de7881d724cf9de054e
Oroko <aoroko09@gmail.com>
2020-07-09 08:40:48 -0400

    Added the API functionality, but the asset mapping is not working

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,34 +1,119 @@
 import React, { Component } from "react";
+import {MoonLoader} from 'react-spinners';
+import assetMapping from '../../assets/assetMapping.json';
 import classes from "./App.module.css";
-import assetMapping from "../../assets/assetMapping.json";
+
 import Card from "../../elements/Card/Card";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import SearchBar from "../../components/SearchBar/SearchBar";
-/**import SearchBar from '../../components/SearchBar/SearchBar';*/
+import WeatherDetails from '../../components/WeatherDetails/WeatherDetails'
 import Preview from "../../components/Preview/Preview";
 import ErrorNotice from "../../components/ErrorNotice/ErrorNotice";
 
-/**<Preview />
-   <ErrorNotice />
-   <assetMapping />
-   These are temporarily placed in the Div to bypass the compile error and test if they are viewable in the browser
-     */
 
-class App extends Component {
-  render() {
-    return (
-      <div className={classes.AppWrapper}>
-        <Header />
-        <SearchBar />
-        <Preview />
-        <Card />
-        <assetMapping />
-        <ErrorNotice />
-        <Footer />
-      </div>
-    );
-  }
-}
+    class App extends Component {
 
-export default App;
+      state = {
+        searchBarInput: '',
+        weatherDetails: {
+          temperature: null,
+          description: ''
+        },
+        loading: false,
+        error: false
+      }
+    
+      // Update state with current search bar input
+      searchBarHandler = (e) => {
+        this.setState({
+          searchBarInput: e.target.value
+        })
+      }
+    
+      // Reset state after clicking on Logo or Try Again button
+      tryAgainHandler = () => {
+        this.setState({
+          searchBarInput: '',
+          weatherDetails: {},
+          error: false
+        })
+      }
+    
+      // Fetch weather information and update state
+      setWeather = () => {
+        const city = this.state.searchBarInput;
+        const API_KEY = "0f602db84741a3d815e3a06011d1f07f";
+        const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+        const URL = API_URL + `?q=${city}&appid=${API_KEY}&units=metric`;
+        this.setState({
+          weatherDetails: {},
+          loading: true,
+          error: false
+        }, () => {
+          // Executed as callback function
+          fetch(URL)
+            .then(res => res.json())
+            .then(data => {
+              // If city exists, update weather details
+              if(data.cod === 200) {
+                this.setState({
+                  weatherDetails: {
+                    temperature: data.main.temp,
+                    description: data.weather[0].main
+                  },
+                  loading: false
+                });
+              } else {
+                // If city doesn't exist, throw error
+                throw data.cod
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              this.setState({
+                loading: false,
+                error: true
+              });
+            });
+        });
+      }
+    
+      render() {
+    
+        // Conditionally render card content
+        let cardContent = <Preview />;
+        if (this.state.loading) {
+          cardContent = <MoonLoader />;
+        } else if (this.state.error) {
+          cardContent = <ErrorNotice onClickHandler={this.tryAgainHandler} />;
+        } else if (this.state.weatherDetails.temperature && this.state.weatherDetails.description !== '') {
+          // Display weather information if temperature and description exists
+          cardContent = <WeatherDetails data={this.state.weatherDetails} />;
+        }
+    
+       return (
+          <div className={classes.AppWrapper}>
+            <Header
+              color={assetMapping.colors[
+                // Set header color based on weather condition; if error, set color to red
+                (this.state.error) ? "error" : this.state.weatherDetails.description
+              ]}
+              onClickHandler={this.tryAgainHandler} />
+            <main className={classes.AppMain}>
+              <SearchBar
+                value={this.state.searchBarInput}
+                onChangeHandler={this.searchBarHandler}
+                onClickHandler={this.setWeather}
+                error={this.state.error} />
+              <Card>
+                {cardContent}
+              </Card>
+            </main>
+            <Footer onClickHandler={this.tryAgainHandler} />
+          </div>
+        );
+      }
+    }
+    
+    export default App;
